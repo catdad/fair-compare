2def05099d88c88fabf52548c6b347ec4751607f
Kiril Vatev <vatev.1@gmail.com>
2020-06-02 17:17:38 -0400

    fixing issue with clicking on currently-selected tab

--- renderer/IndexTabs/IndexTabs.js
+++ renderer/IndexTabs/IndexTabs.js
@@ -41,9 +41,9 @@ function App() {
     tabs.forEach(tab => {
       if (tab === TAB) {
         tab.selected = true;
-        tab.frame.style.top = '200vh';
       } else {
         tab.selected = false;
+        tab.frame.style.top = '200vh';
       }
     });
 
