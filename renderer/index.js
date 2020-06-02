09a76bc87e63119535a55ae336eeb0d24c3cccbf
Kiril Vatev <vatev.1@gmail.com>
2020-06-01 20:52:44 -0400

    starting to add tab support

--- renderer/index.js
+++ renderer/index.js
@@ -1,4 +1,11 @@
 const { html, render } = require('./tools/ui.js');
 const App = require('./App/App.js');
 
-render(html`<${App}/>`, document.querySelector('#app'));
+const tabs = require('./Chrome/tabs.js')({
+  tabs: document.querySelector('#tabs'),
+  app: document.querySelector('#app')
+});
+
+const [app] = tabs.add({ title: 'Main' });
+
+render(html`<${App}/>`, app);
