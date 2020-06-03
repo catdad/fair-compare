9213ed9f489d652e0f8de132a31d7d572368b488
Kiril Vatev <vatev.1@gmail.com>
2020-06-03 19:23:18 -0400

    implementing generating a tolerance image

--- renderer/IndexImage/Tolerance.js
+++ renderer/IndexImage/Tolerance.js
@@ -1,13 +1,74 @@
+const pixelmatch = require('pixelmatch');
+
 const { html, useState, useEffect } = require('../tools/ui.js');
 
+const Image = require('./Image.js');
+
+const loadUrl = (img, url) => {
+  return new Promise((resolve, reject) => {
+    img.onload = () => resolve();
+    img.onerror = e => reject(e);
+    img.src = url;
+  });
+};
+
+const readImageData = async (filepath) => {
+  const img = new window.Image();
+  await loadUrl(img, filepath);
+  const { naturalWidth: tw, naturalHeight: th } = img;
+  const { ctx } = getCanvas(tw, th);
+
+  ctx.drawImage(img, 0, 0, tw, th);
+
+  return ctx.getImageData(0, 0, tw, th);
+};
+
+const getCanvas = (width, height) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext('2d');
+
+  return { canvas, ctx };
+};
+
+const computeTolerance = async (left, right) => {
+  const [leftData, rightData] = await Promise.all([
+    readImageData(left),
+    readImageData(right)
+  ]);
+
+  const { canvas, ctx } = getCanvas(leftData.width, leftData.height);
+  const resultData = ctx.createImageData(leftData.width, leftData.height);
+
+  pixelmatch(leftData.data, rightData.data, resultData.data, leftData.width, leftData.height, { threshold: 0.1 });
+
+  ctx.fillRect(0, 0, 100, 100);
+  ctx.putImageData(resultData, 0, 0);
+
+  return { leftData, rightData, resultData, imageUrl: canvas.toDataURL() };
+};
+
 function Tolerance({ left, right }) {
-  const [tolerance, setTolerance] = useState(null);
+  const [data, setData] = useState({ tolerance: null, left: null, right: null, imageUrl: null });
 
   useEffect(() => {
+    console.time('tolerance');
+    computeTolerance(left, right).then(result => {
+      console.timeEnd('tolerance');
 
+      setData({
+        left: result.leftData,
+        right: result.rightData,
+        tolerance: result.resultData,
+        imageUrl: result.imageUrl
+      });
+    }).catch(err => {
+      console.error(err);
+    });
   }, []);
 
-  return tolerance ? html`<Image title="Tolerance" filepath=${tolerance} />` : html``;
+  return data.imageUrl ? html`<${Image} title="Tolerance" filepath=${data.imageUrl} />` : html`<div>Loading...</div>`;
 }
 
 module.exports = Tolerance;
