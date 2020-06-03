f919cae077a8e7bbebea05b978a2c9502a544984
Kiril Vatev <vatev.1@gmail.com>
2020-06-03 07:40:05 -0400

    adding the ability to detect images or text and open the correct diff tab

--- renderer/IndexDirectory/IndexDirectory.js
+++ renderer/IndexDirectory/IndexDirectory.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const fs = require('fs-extra');
+const FileType = require('file-type');
+
 const { html, css, useState } = require('../tools/ui.js');
 const { ipcRenderer } = require('electron');
 const Directory = require('../Directory/Directory.js');
@@ -16,13 +19,28 @@ function App() {
   const [dir2, setDir2] = useState({ base: null, files: [] });
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const onOpen = (file) => {
+  const onOpen = (dir) => async (file) => {
     const left = fileInDir(dir1, file);
     const right = fileInDir(dir2, file);
 
+    // TODO handle errors
+    const result = await FileType.fromFile(fileInDir(dir, file));
+    const { mime } = result || { mime: 'text/plain' };
+
+    const route = mime.split('/')[0];
+
+    if (!['text', 'image'].includes(route)) {
+      // TODO handle this error better
+
+      // eslint-disable-next-line no-console
+      console.error(`unsupported diff mime: ${mime}`);
+      return;
+    }
+
+
     const data = {
       title: path.basename(file),
-      route: 'text'
+      route
     };
 
     if (left) {
@@ -38,8 +56,8 @@ function App() {
 
   return html`
     <div class=main>
-      <${Directory} dir=${dir1} setDir=${setDir1} selected=${selectedFile} onSelect=${setSelectedFile} onOpen=${onOpen} />
-      <${Directory} dir=${dir2} setDir=${setDir2} selected=${selectedFile} onSelect=${setSelectedFile} onOpen=${onOpen} />
+      <${Directory} dir=${dir1} setDir=${setDir1} selected=${selectedFile} onSelect=${setSelectedFile} onOpen=${onOpen(dir1)} />
+      <${Directory} dir=${dir2} setDir=${setDir2} selected=${selectedFile} onSelect=${setSelectedFile} onOpen=${onOpen(dir2)} />
     </div>
   `;
 }
