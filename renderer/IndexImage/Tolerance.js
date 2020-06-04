bb92483a67fc57a0c6e4b2ae2983759c6a185d64
Kiril Vatev <vatev.1@gmail.com>
2020-06-03 20:02:23 -0400

    extracting diff logic to a util file, so it can be reused later

--- renderer/IndexImage/Tolerance.js
+++ renderer/IndexImage/Tolerance.js
@@ -1,53 +1,7 @@
-const pixelmatch = require('pixelmatch');
-
 const { html, useState, useEffect } = require('../tools/ui.js');
+const imageDiff = require('../tools/image-diff.js');
 
 const Image = require('./Image.js');
-
-const loadUrl = (img, url) => {
-  return new Promise((resolve, reject) => {
-    img.onload = () => resolve();
-    img.onerror = e => reject(e);
-    img.src = url;
-  });
-};
-
-const readImageData = async (filepath) => {
-  const img = new window.Image();
-  await loadUrl(img, filepath);
-  const { naturalWidth: tw, naturalHeight: th } = img;
-  const { ctx } = getCanvas(tw, th);
-
-  ctx.drawImage(img, 0, 0, tw, th);
-
-  return ctx.getImageData(0, 0, tw, th);
-};
-
-const getCanvas = (width, height) => {
-  const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-  const ctx = canvas.getContext('2d');
-
-  return { canvas, ctx };
-};
-
-const computeTolerance = async (left, right) => {
-  const [leftData, rightData] = await Promise.all([
-    readImageData(left),
-    readImageData(right)
-  ]);
-
-  const { canvas, ctx } = getCanvas(leftData.width, leftData.height);
-  const resultData = ctx.createImageData(leftData.width, leftData.height);
-
-  pixelmatch(leftData.data, rightData.data, resultData.data, leftData.width, leftData.height, { threshold: 0.1 });
-
-  ctx.fillRect(0, 0, 100, 100);
-  ctx.putImageData(resultData, 0, 0);
-
-  return { leftData, rightData, resultData, imageUrl: canvas.toDataURL() };
-};
 
 function Tolerance({ left, right, cache, setCache }) {
   const [data, setData] = useState({ tolerance: null, left: null, right: null, imageUrl: null });
@@ -59,7 +13,7 @@ function Tolerance({ left, right, cache, setCache }) {
     }
 
     console.time('tolerance');
-    computeTolerance(left, right).then(result => {
+    imageDiff({ left, right }).then(result => {
       console.timeEnd('tolerance');
 
       const data = {
---
+++ renderer/tools/image-diff.js
@@ -0,0 +1,71 @@
+const pixelmatch = require('pixelmatch');
+
+const loadUrl = (img, url) => {
+  return new Promise((resolve, reject) => {
+    img.onload = () => resolve();
+    img.onerror = e => reject(e);
+    img.src = url;
+  });
+};
+
+const readImageData = async (filepath) => {
+  console.time('draw');
+  const img = new window.Image();
+  await loadUrl(img, filepath);
+  const { naturalWidth: tw, naturalHeight: th } = img;
+  const { ctx } = getCanvas(tw, th);
+
+  ctx.drawImage(img, 0, 0, tw, th);
+  console.timeEnd('draw');
+
+  console.time('draw-data');
+  const data = ctx.getImageData(0, 0, tw, th);
+  console.timeEnd('draw-data');
+
+  return data;
+};
+
+const getCanvas = (width, height) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext('2d');
+
+  return { canvas, ctx };
+};
+
+const computeTolerance = async (left, right) => {
+  console.time('read');
+  const [leftData, rightData] = await Promise.all([
+    readImageData(left),
+    readImageData(right)
+  ]);
+  console.timeEnd('read');
+
+  console.time('diff-create');
+  const { canvas, ctx } = getCanvas(leftData.width, leftData.height);
+  const resultData = ctx.createImageData(leftData.width, leftData.height);
+  console.timeEnd('diff-create');
+
+  console.time('diff');
+  const pixels = pixelmatch(leftData.data, rightData.data, resultData.data, leftData.width, leftData.height, { threshold: 0.05 });
+  console.timeEnd('diff');
+
+  console.time('diff-put');
+  ctx.putImageData(resultData, 0, 0);
+  console.timeEnd('diff-put');
+
+  return { leftData, rightData, pixels, resultData, resultCanvas: canvas };
+};
+
+module.exports = async ({ left, right, threshold = 0.05, url = true, pixels = true }) => {
+  const result = await computeTolerance(left, right);
+
+  if (url) {
+    console.time('diff-url');
+    result.imageUrl = result.resultCanvas.toDataURL();
+    console.timeEnd('diff-url');
+  }
+
+  return result;
+};
