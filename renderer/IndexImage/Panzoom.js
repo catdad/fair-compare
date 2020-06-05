83e95ebbf517bf0a8e5a9fdbd7e8f44896eeeb07
Kiril Vatev <vatev.1@gmail.com>
2020-06-05 18:13:27 -0400

    persisting panzoom state so that it can be reused when switching image modes

--- renderer/IndexImage/Panzoom.js
+++ renderer/IndexImage/Panzoom.js
@@ -3,6 +3,7 @@ const { html, useContext, useEffect, useRef } = require('../tools/ui.js');
 const Cache = require('./cache.js');
 
 const KEY = '__x_panzoom';
+const maxScale = 4;
 
 module.exports = function Panzoom ({ children, view }) {
   const zoom = useRef(null);
@@ -17,23 +18,39 @@ module.exports = function Panzoom ({ children, view }) {
       return;
     }
 
-    const win = zoom.current.getBoundingClientRect();
-    const box = view.getBoundingClientRect();
+    const data = cache.get(KEY);
 
-    const startScale = Math.min(win.width / box.width, win.height / box.height, 1) * 0.98;
-    const startX = -((box.width / 2) - (win.width / 2));
-    const startY = -((box.height / 2) - (win.height / 2));
+    if (data) {
+      zoom.current[KEY] = panzoom(zoom.current, {
+        maxScale,
+        ...data
+      });
+    } else {
+      const win = zoom.current.getBoundingClientRect();
+      const box = view.getBoundingClientRect();
 
-    const instance = zoom.current[KEY] = panzoom(zoom.current, {
-      maxScale: 4,
-      startScale,
-      startX,
-      startY
-    });
+      const startScale = Math.min(win.width / box.width, win.height / box.height, 1) * 0.98;
+      const startX = -((box.width / 2) - (win.width / 2));
+      const startY = -((box.height / 2) - (win.height / 2));
+
+      zoom.current[KEY] = panzoom(zoom.current, {
+        maxScale,
+        startScale,
+        startX,
+        startY
+      });
+    }
+
+    const instance = zoom.current[KEY];
 
     zoom.current.addEventListener('wheel', instance.zoomWithWheel);
 
     return () => {
+      const { x: startX, y: startY } = instance.getPan();
+      const startScale = instance.getScale();
+
+      cache.set(KEY, { startX, startY, startScale });
+
       zoom.current.setAttribute('style', {});
       zoom.current.removeEventListener('wheel', instance.zoomWithWheel);
       instance.destroy();
