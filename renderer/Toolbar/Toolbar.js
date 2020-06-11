29f0b50a5c489c5dfa3ed20603565ea6a7ad2eec
Kiril Vatev <vatev.1@gmail.com>
2020-06-11 18:46:34 -0400

    cleaning up toolbar styles

--- renderer/Toolbar/Toolbar.js
+++ renderer/Toolbar/Toolbar.js
@@ -2,7 +2,7 @@ const { html, css, setRootVar } = require('../tools/ui.js');
 
 css('./Toolbar.css');
 
-const SIZE = '2rem';
+const SIZE = '2.4rem';
 
 function Toolbar({ children, heightVar = 'toolbar-height' }) {
   setRootVar(heightVar, SIZE);
