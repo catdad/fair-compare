5d27c8de2d5edcdd2ea044a6f4c902e3c0e20b29
Kiril Vatev <vatev.1@gmail.com>
2020-06-22 20:56:36 -0400

    implementing side by side text diff

--- renderer/IndexText/IndexText.js
+++ renderer/IndexText/IndexText.js
@@ -1,13 +1,22 @@
 const { html, css } = require('../tools/ui.js');
 const fs = require('fs-extra');
+const { diffLines } = require('diff');
 
 css('./IndexText.css');
 
-function File({ title = 'no file available', text = '' }) {
+function FileSide({ title = 'no file available', chunks = [], side }) {
+  const lines = chunks.map(({ added, removed, value }) => {
+    const classes = side === 'left' ?
+      removed ? 'red' : added ? 'hide' : '' :
+      added ? 'green' : removed ? 'hide' : '';
+
+    return html`<span class=${classes}>${value}</span>`;
+  });
+
   return html`
     <div class=file>
       <p>${title}</p>
-      <pre>${text}</pre>
+      <pre>${lines}</pre>
     </div>
   `;
 }
@@ -17,18 +26,20 @@ function App({ left, right }) {
 
   if (left) {
     // TODO don't do this sync
-    leftText = fs.readFileSync(left, 'utf-8');
+    leftText = fs.readFileSync(left, 'utf-8') || '';
   }
 
   if (right) {
     // TODO don't do this sync
-    rightText = fs.readFileSync(right, 'utf-8');
+    rightText = fs.readFileSync(right, 'utf-8') || '';
   }
+
+  const diff = diffLines(leftText, rightText);
 
   return html`
     <div class=main>
-      <${File} title=${left} text=${leftText} />
-      <${File} title=${right} text=${rightText} />
+      <${FileSide} title=${left} chunks=${diff} side=left />
+      <${FileSide} title=${right} chunks=${diff} side=right />
     </div>
   `;
 }
