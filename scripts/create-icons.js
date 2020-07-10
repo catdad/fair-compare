7f0a19311c9215b8f8200d8ec39001fd4bab6f48
Kiril Vatev <vatev.1@gmail.com>
2020-07-10 14:31:47 -0400

    expanding use tags in svg icons so that they can be rendered correctly

--- scripts/create-icons.js
+++ scripts/create-icons.js
@@ -4,6 +4,7 @@ const root = require('rootrequire');
 const renderSvg = require('svg-render');
 const pngToIco = require('png-to-ico');
 const { convert: icnsConvert } = require('@fiahfy/icns-convert');
+const cheerio = require('cheerio');
 
 const timing = require('../lib/timing.js')('prep:icons');
 
@@ -24,10 +25,35 @@ const createIcns = async svg => await icnsConvert([
   await render(svg, 1024)
 ]);
 
+// this is a bad idea, but canvas can't render the use tags correctly
+const preprocess = svg => {
+  const $ = cheerio.load(svg.toString(), { xmlMode: true });
+
+  // replace use tags with the a copy of the def item they are using
+  $('use').each((i, elem) => {
+    const $elem = $(elem);
+    const id = $elem.attr('href');
+    const shape = $(id).clone();
+    shape.removeAttr('id');
+
+    for (let key in $elem.get(0).attribs) {
+      if (key === 'href') {
+        continue;
+      }
+
+      shape.attr(key, $elem.attr(key));
+    }
+
+    $elem.replaceWith(shape);
+  });
+
+  return Buffer.from($.xml());
+};
+
 timing({
   label: 'create icons',
   func: async () => {
-    const svg = await read(path.resolve(root, 'assets/icon.svg'));
+    const svg = preprocess(await read(path.resolve(root, 'assets/icon.svg')));
 
     await write(dist('icon.svg'), svg);
     await write(dist('icon.png'), await render(svg, 512));
