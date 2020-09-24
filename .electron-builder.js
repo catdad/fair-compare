71238c26234b6a246d1fd2d0a673b831649d70cc
Kiril Vatev <vatev.1@gmail.com>
2020-09-24 18:02:58 -0400

    using a standalone electron-builder config file with logic in it instead of hardcoded values in package.json

---
+++ .electron-builder.js
@@ -0,0 +1,52 @@
+const { name, productName, appId, version } = require('./package.json');
+const icon = require('./lib/icon.js')('build');
+const iconGlob = require('./lib/icon.js')('glob');
+
+const fileName = productName.replace(/\s/g, '');
+
+module.exports = {
+  appId,
+  productName,
+  buildVersion: version,
+  files: [
+    '!assets/*',
+    '!scripts/*',
+    '!.*'
+  ],
+  mac: {
+    icon,
+    target: [
+      'dmg'
+    ],
+    darkModeSupport: true
+  },
+  dmg: {
+    icon,
+    artifactName: `${fileName}-v\${version}-MacOS-setup.\${ext}`
+  },
+  win: {
+    icon,
+    target: [
+      'nsis',
+      'portable'
+    ]
+  },
+  nsis: {
+    artifactName: `${fileName}-v\${version}-Windows-setup.\${ext}`
+  },
+  portable: {
+    artifactName: `${fileName}-v\${version}-Windows-portable.\${ext}`
+  },
+  linux: {
+    icon,
+    target: [
+      'AppImage'
+    ],
+    executableName: productName,
+    category: 'Network',
+    asarUnpack: [ iconGlob ]
+  },
+  appImage: {
+    artifactName: `${fileName}-v\${version}-Linux.\${ext}`
+  }
+};
